f7c315a1bca3358cf0433f029b6e587bbdaa5a5c
Sean Killeen <SeanKilleen@gmail.com>
2018-08-07 00:15:25 -0400

    move stuff to shared project

--- 2018-08_AkkaIntro/code/src/Shared/Actors/RandomNumberAfterRandomTimeWorker.cs
+++ 2018-08_AkkaIntro/code/src/Shared/Actors/RandomNumberAfterRandomTimeWorker.cs
@@ -11,6 +11,7 @@ namespace Shared.Actors
 
         public RandomNumberAfterRandomTimeWorker()
         {
+            Console.WriteLine($"created at {Self.Path}");
             _consoleWriterActorRef = Context.ActorOf(Props.Create<ConsoleWriterActor>(), "consoleWriter");
             Console.WriteLine("console actor created at {0}", _consoleWriterActorRef.Path);
 
