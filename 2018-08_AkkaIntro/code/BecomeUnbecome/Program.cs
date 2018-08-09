8d735d15159639e8d3e8d4751b599ba471449e19
Sean Killeen <SeanKilleen@gmail.com>
2018-08-09 18:41:00 -0400

    Add BecomeUnbecome demo

---
+++ 2018-08_AkkaIntro/code/BecomeUnbecome/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Akka.Actor;
+using Shared.Actors;
+using Shared.Messages;
+
+namespace BecomeUnbecome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var actorSystem = ActorSystem.Create("becomeUnbecomeDemo");
+            var variableWriter = actorSystem.ActorOf<VariableConsoleWriterActor>();
+
+            Console.WriteLine("Say some things!");
+            while (true)
+            {
+                var text = Console.ReadLine();
+                variableWriter.Tell(new WriteSomethingMessage(text), ActorRefs.Nobody);
+            }
+        }
+    }
+}
