467546be5b7470cbaf59ff088731cb1fdf6b3131
Sean Killeen <SeanKilleen@gmail.com>
2019-02-28 18:07:19 -0500

    copy from prior talk

---
+++ 2019-03_AkkaIntro/code/src/HelloWorld/HelloWorldActor.cs
@@ -0,0 +1,29 @@
+using System;
+using Akka;
+using Akka.Actor;
+using Shared;
+using Shared.Actors;
+using Shared.Messages;
+
+namespace HelloWorld
+{
+    public class HelloWorldActor : ReceiveActor
+    {
+        private readonly IActorRef _consoleWriter;
+
+        public HelloWorldActor()
+        {
+            _consoleWriter = Context.ActorOf<ConsoleWriterActor>();
+
+            Receive<HelloWorldMessage>(message =>
+            {
+                _consoleWriter.Tell(new WriteSomethingMessage($"Hello, {message.Name}"), Self);
+            });
+
+            Receive<FinishedMessage>(message =>
+            {
+                _consoleWriter.Tell(new WriteSomethingMessage("I guess we're done here!"));
+            });
+        }
+    }
+}
