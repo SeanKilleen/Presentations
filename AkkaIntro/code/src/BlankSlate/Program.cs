2e4658d6a6fb47526fcd17f1d2bd80f212afc842
Sean Killeen <SeanKilleen@gmail.com>
2019-05-27 21:28:47 -0400

    Use proper async main

--- AkkaIntro/code/src/BlankSlate/Program.cs
+++ AkkaIntro/code/src/BlankSlate/Program.cs
@@ -7,17 +7,13 @@ namespace BlankSlate
 
     class Program
     {
-        static void Main(string[] args)
-        {
-            Task.Run(() => MainAsync(args)).Wait();
-        }
-
-        static async Task MainAsync(string[] args)
+        static async Task Main(string[] args)
         {
             var actorSystem = ActorSystem.Create("BlankSlate");
 
             Console.WriteLine("Sitting here doing my thing");
-            Console.ReadLine();
+
+            await actorSystem.WhenTerminated;
         }
     }
 }
