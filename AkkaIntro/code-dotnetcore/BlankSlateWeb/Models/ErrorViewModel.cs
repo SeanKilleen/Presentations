bf68480e6445d96baea6de9ea17426db8dc8dfb3
Sean Killeen <SeanKilleen@gmail.com>
2020-10-13 18:00:16 -0400

    Add BlankSlateWeb

---
+++ AkkaIntro/code-dotnetcore/BlankSlateWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BlankSlateWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
