1d1feb058028e1a271f9079f30b42c9632a8b60d
Sean Killeen <SeanKilleen@gmail.com>
2021-03-09 17:45:46 -0500

    List the nuget packages
    Make it a little easier, why not :)

--- joy-of-functional-mdx/code/FromScratch/Class1.cs
+++ joy-of-functional-mdx/code/FromScratch/Class1.cs
@@ -3,9 +3,13 @@
 namespace FromScratch
 {
     public class Class1
-    { 
+    {
         // TODO: SpecFlow Extension
-        // TODO: NuGet Packages
+        // TODO: NuGet Packages:
+            // SpecFlow
+            // SpecFlow.NUnit
+            // SpecFlow.Tools.MsBuild.Generation
+            // NUnit3TestAdapter
         // TODO: Add an example specflow project
     }
 }
